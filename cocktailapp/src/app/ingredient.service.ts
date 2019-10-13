bb9fc2718559b957b7d7137e3c5bc85f6bd875c3
John Connor <hrgplumer@gmail.com>
2019-10-13 16:15:04 -0400

    Disallow duplicate ingredients being added to the ingredients list

--- cocktailapp/src/app/ingredient.service.ts
+++ cocktailapp/src/app/ingredient.service.ts
@@ -19,8 +19,10 @@ export class IngredientService {
      * @param ing The Ingredient to add.
      */
     addIngredient(ing: Ingredient) {
-        this.ingredientList.push(ing);
-        this.updateIngredients();
+        if (!this.ingredientList.find(i => i.name === ing.name)) {
+            this.ingredientList.push(ing);
+            this.updateIngredients();
+        }
     }
 
     /**
@@ -43,6 +45,6 @@ export class IngredientService {
 
     private joinList<T>(list: Array<T>, char: string, mapper): string {
         return list.map(mapper).join(char);
-      }
+    }
 
 }
